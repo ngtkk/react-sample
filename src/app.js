51a74e9e5e7b13e22eb1621c79ec2d05cdcd4000
Nagata <ngtkko@gmail.com>
2018-06-21 14:00:14 +0900

    クリックした回数

--- src/app.js
+++ src/app.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  onClickButton() {
-    alert("警告!")
-    
-  }
-
-render(){
-  return <button onClick={this.onClickButton}>押す</button>;
+  
+constructor(){
+ super() 
+ this.state = {
+  count: 0 
 }
+  this.onClickButton = this.onClickButton.bind(this) 
+}
+
+ onClickButton(){
+  let { count } = this.state
+   this.setState({ count: count + 1}) 
+}
+ render() {
+
+return (
+ <div><h1>{`クリックした回数: ${this.state.count}`}</h1>
+  <button onClick ={ this.onClickButton}> 押す </button> </div>
+  )
+   }
 }
 
 ReactDOM.render(
