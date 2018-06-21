f98374c603f2f90b47ed9e7e16174dbd960aef59
Nagata <ngtkko@gmail.com>
2018-06-21 13:35:36 +0900

    警告！

--- src/app.js
+++ src/app.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
-    return <h1>Hello {this.props.name}</h1>;
+  onClickButton() {
+    alert("警告!")
     
   }
+
+render(){
+  return <button onClick={this.onClickButton}>押す</button>;
+}
 }
 
 ReactDOM.render(
-  <App name={"Bob"} />,
-  document.getElementById("app")  
+<App />,
+document.getElementById("app")
+
 )
